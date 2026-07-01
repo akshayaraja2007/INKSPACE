06573504910d7ddd38f66761e729979acb1bd4e4
akshayaraja2007 <akshayarajae@gmail.com>
2026-07-01 19:54:09 +0530

    Implement JWT authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const db = require("../config/db");
 
@@ -41,7 +42,63 @@ const register = async (req, res) => {
 
     }
 };
+const login = (req, res) => {
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: "Email and Password are required"
+        });
+    }
+
+    const sql = "SELECT * FROM users WHERE email = ?";
+
+    db.query(sql, [email], async (err, results) => {
+
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+
+        if (results.length === 0) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const user = results[0];
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                username: user.username
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        res.status(200).json({
+            message: "Login Successful",
+            token
+        });
+
+    });
+
+};
 
 module.exports = {
-    register
+    register,
+     login
 };
