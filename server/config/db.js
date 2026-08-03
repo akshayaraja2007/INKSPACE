2adf0ae6937ee2d2e4c9eba2988ddd80b4d379e8
akshayaraja2007 <akshayarajae@gmail.com>
2026-08-03 19:40:16 +0530

    removed local hosts

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,25 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "inkspace"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
+
     if (err) {
         console.error("Database connection failed:", err);
         return;
     }
-    
+
+    console.log("✅ MySQL Connected");
+
 });
 
 module.exports = db;
