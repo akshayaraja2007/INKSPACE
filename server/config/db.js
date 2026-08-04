f5456734b99f33c0047abdc08a5bd7debef93613
akshayaraja2007 <akshayarajae@gmail.com>
2026-08-04 20:43:12 +0530

    Debug DB environment

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,8 @@
 const mysql = require("mysql2");
-
+console.log("DB_HOST =", process.env.DB_HOST);
+console.log("DB_PORT =", process.env.DB_PORT);
+console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_NAME =", process.env.DB_NAME);
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
